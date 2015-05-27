7d0abe82841204d10fe2897620e569cb980324aa
lishiyi <lishiyiiii@gmail.com>
2015-05-26 20:51:11 -0400

    #66 PlusOne

--- src/edu/nyu/lishiyi/PlusOne.java
+++ src/edu/nyu/lishiyi/PlusOne.java
@@ -1,4 +1,7 @@
 package edu.nyu.lishiyi;
+
+import java.util.Arrays;
+
 /**
  * #66 Plus One
  * @author Shiyi
@@ -15,6 +18,30 @@ public class PlusOne {
 			return null;
 		}
 		
+		
+		for(int i = digits.length - 1 ; i >= 0 ; i --){
+			int d = digits[i] + 1;
+			if( d < 10 ){
+				digits[i] = d;
+				break;
+			}
+			else{
+				digits[i] = 0;
+			}
+		}
+		
+		if(digits[0] != 0){
+			return digits;
+		}
+		else{
+			int[] temp = new int[digits.length+1];
+			temp[0] = 1;
+			for(int i = 0; i<digits.length ; i++){
+				temp[i+1]=digits[i];
+			}
+			return temp;
+		}
+		/*
 		boolean needMoreDigit = true;
 		for(int digit : digits){
 			if(digit != 9 ){
@@ -28,6 +55,7 @@ public class PlusOne {
 			num += Math.pow(10, len - i - 1) * digits[i];
 		}
 		num += 1;
+		
 		
 		
 		System.out.println(num);
@@ -44,15 +72,18 @@ public class PlusOne {
 		System.out.println(temp.toString());
 		
 		return temp;
+		*/
     }
+	
 	
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		
 		int[] a = new int[] {1,2,3,4,5,6,7,8,9};
-		plusOne(a);
+		System.out.println(Arrays.toString(plusOne(a)));
+		
 		int[] b = new int[] {6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3};
-		plusOne(b);
+		System.out.println(Arrays.toString(plusOne(b)));
 		
 		
 	}
