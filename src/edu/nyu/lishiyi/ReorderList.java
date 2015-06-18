fc225fbf5b48e463d1ec321be3415a1514da56a6
lishiyi <lishiyiiii@gmail.com>
2015-06-17 20:13:56 -0400

    #143 Reorder List

--- src/edu/nyu/lishiyi/ReorderList.java
+++ src/edu/nyu/lishiyi/ReorderList.java
@@ -14,9 +14,63 @@ public class ReorderList {
 
 	public void reorderList(ListNode head) {
         
-		ListNode dummy = new ListNode(0);
-		dummy.next = head;
+		//If the linked list is null or only has one element, it will be not applicable
+		if(head==null||head.next==null){
+			return;
+		}
+		
+		//We set to pointers, one fast, one slow.
+		ListNode fast = head;
+		
+		ListNode slow = head;
+		
+		//Fast runs 2 times faster than the Slow
+		while(fast.next!=null && fast.next.next!=null){
+			
+			slow = slow.next;
+			
+			fast = fast.next.next;
+		}
+		
+		//head1 is the original head
+		ListNode head1 = head;
+		
+		//head2 is the on the second half of the ll (n/2 + 1)
+		ListNode head2 = slow.next;
+		
+		//Set the next of slow to null make sure head1 has only first half of the ll
+		//That is no more elements after the half
+		slow.next = null;
+		
+		head2 = reverse(head2);
+		
+		while(head1!=null && head2!=null){
+			
+			
+		}
+		
 		
 		
     }
+	
+	public ListNode reverse(ListNode head){
+		
+		ListNode prev = null;
+		
+		ListNode current = head;
+		
+		while(current!=null){
+			
+			ListNode next = current.next;
+			
+			current.next = prev;
+			
+			prev = current;
+			
+			current = next;
+			
+		}
+		
+		return prev;
+	}
 }
--- src/edu/nyu/lishiyi/ReverseLinkedList.java
+++ src/edu/nyu/lishiyi/ReverseLinkedList.java
@@ -42,6 +42,8 @@ public class ReverseLinkedList {
         }
 
         return prev;
+        
+        
     }
 	
 	
