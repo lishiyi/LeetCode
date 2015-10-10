853416fe6552e5bafe25671c4eba2847e828b0c6
lishiyi <lishiyiiii@gmail.com>
2015-10-09 21:54:07 -0400

    Invert Binary Tree

---
+++ src/edu/nyu/lishiyi/InvertBinaryTree.java
@@ -0,0 +1,40 @@
+Invert Binary Tree
+ My Submissions Question Solution 
+Total Accepted: 41662 Total Submissions: 104711 Difficulty: Easy
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+        	return null;
+        }
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
