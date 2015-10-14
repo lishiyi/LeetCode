ce14451bc190cb7af4c131996c4830d824b0c0dd
lishiyi <lishiyiiii@gmail.com>
2015-10-13 20:16:01 -0400

    Rotate Array

--- src/edu/nyu/lishiyi/RotateArray.java
+++ src/edu/nyu/lishiyi/RotateArray.java
@@ -1,5 +1,5 @@
 Rotate Array
- My Submissions Question Solution 
+My Submissions Question Solution 
 Total Accepted: 45941 Total Submissions: 244968 Difficulty: Easy
 Rotate an array of n elements to the right by k steps.
 
@@ -11,3 +11,23 @@ Try to come up as many solutions as you can, there are at least 3 different ways
 [show hint]
 
 Related problem: Reverse Words in a String II
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums == null || nums.length == 0 || k == 0) return;
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end){
+    	while(start < end){
+    		int temp = nums[end];
+    		nums[end] = nums[start];
+    		nums[start] = temp;
+    		start++;
+    		end--;
+    	}
+    }
+}
